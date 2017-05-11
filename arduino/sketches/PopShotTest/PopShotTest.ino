bfcd492b1db5e0bbf6df16be5fa1563c675129df
Michael Bunsen <notbot@gmail.com>
2017-05-11 08:57:43 -0700

    Change example to a basic counter

--- arduino/sketches/PopShotTest/PopShotTest.ino
+++ arduino/sketches/PopShotTest/PopShotTest.ino
@@ -7,30 +7,50 @@ const int buttonPin = 38;
 const int sensorPin = A3;
 int buttonState = digitalRead(buttonPin);
 int sensorValue = 0;
-int counter = 0;
+int counter = 12345;
+long startTime;
+long elapsedTime;
 
 void setup() {
   pinMode(buttonPin, INPUT);
   
   Serial.begin(9600);
-  delay(1000);
-  
-  disp.setNumDigits(2);
-  delay(1000);
+  delay(200);
+  Serial.println("Setting up display...");
+
+  disp.setNumDigits(5);
+  delay(200);
   
   disp.clearDisp();
-  delay(1000);
+  delay(200);
+
+  Serial.println("Ready!");
+  startTime = millis();
 }
 
 void loop() {  
 
 buttonState = digitalRead(buttonPin);
-sensorValue = analogRead(sensorPin);
+//sensorValue = analogRead(sensorPin);
+
+elapsedTime = millis() - startTime;
+// divide by 1000 to convert to seconds - then cast to an int to print
+if (elapsedTime > 10) {
+    counter += 1;
+    Serial.print( counter );
+    Serial.print(" / ");
+    startTime = millis();
+    Serial.println( (int)(startTime / 1000L));
+    // @TODO blink LED
+    //digitalWrite(13, HIGH);
+    disp.write(counter);
+}
 
 if (buttonState == HIGH) {
   counter++;
-  disp.write(sensorValue);
-  delay(1000);
+  //disp.write(counter);
+  Serial.println(counter);
+  delay(50);
 }
 
 
